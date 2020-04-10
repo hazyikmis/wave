fd0c0e4a60cb6980ccb057bc473765351868d809
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2020-04-10 02:43:16 +0200

    home page slider added

---
+++ client/src/components/Home/home_sider.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Slider from "react-slick";
+import { GenericButton } from "../utils/buttons";
+
+export const HomeSlider = (props) => {
+  const slides = [
+    {
+      img: "/images/featured/featured_home.jpg",
+      lineOne: "Fender",
+      lineTwo: "Custome shop",
+      linkTitle: "Shop now",
+      linkTo: "/shop",
+    },
+    {
+      img: "/images/featured/featured_home_2.jpg",
+      lineOne: "B-Stock",
+      lineTwo: "Awesome discounts",
+      linkTitle: "View offers",
+      linkTo: "/shop",
+    },
+  ];
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
+  const generateSlides = () =>
+    slides
+      ? slides.map((item, i) => (
+          <div key={i}>
+            <div
+              className="featured_image"
+              style={{
+                background: `url(${item.img})`,
+                height: `${window.innerHeight}px`,
+              }}
+            >
+              <div className="featured_action">
+                <div className="tag title">{item.lineOne}</div>
+                <div className="tag low_title">{item.lineTwo}</div>
+                <div>
+                  <GenericButton
+                    type="default"
+                    title={item.linkTitle}
+                    linkTo={item.linkTo}
+                    addStyles={{
+                      margin: "10px 0 0 0",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      : null;
+
+  return (
+    <div className="featured_container">
+      <Slider {...settings}>{generateSlides()}</Slider>
+    </div>
+  );
+};
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { HomeSlider } from "./home_sider";
+import { HomePromotion } from "./home_promotion";
 
 export default class index extends Component {
   render() {
     return (
       <div>
-        <div>HOME</div>
+        <div>
+          <HomeSlider />
+          <HomePromotion />
+        </div>
       </div>
     );
   }
